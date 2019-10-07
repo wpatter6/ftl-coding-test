7016cd68aad6f95ea5b0a70c77d3ffdb75d88f6a
Will Patterson <wpatter6@gmail.com>
2019-10-07 12:56:37 -0600

    Add modal component, numeric directive

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
-import Vue from 'vue';
-import App from './App.vue';
+import Vue from 'vue'
+import App from './App.vue'
+import VModal from 'vue-js-modal'
 
-Vue.config.productionTip = false;
+Vue.use(VModal)
+
+Vue.directive('numericOnly', {
+  bind(el: HTMLElement) {
+    const regex = /^[0-9]*$/
+    if (el instanceof HTMLInputElement) {
+      el.addEventListener('keyup', () => {
+        if (!regex.test(el.value)) {
+          el.value = el.value.slice(0, -1)
+        }
+      })
+    }
+  },
+})
+
+Vue.config.productionTip = false
 
 new Vue({
-  render: (h) => h(App),
-}).$mount('#app');
+  render: h => h(App),
+}).$mount('#app')
